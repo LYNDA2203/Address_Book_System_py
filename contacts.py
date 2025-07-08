3448934b132107cc9dc9a750eef6aece9ee7a9be
LYNDA2203 <lindam.rita@gmail.com>
2025-07-08 12:54:31 +0530

    [LYNDA] feat: Added the new class contacts in the Address book repo

---
+++ contacts.py
@@ -0,0 +1,24 @@
+class Contacts:
+    #creating the init to get the contacts details
+    def __init__(self,first_name,last_name,address,city,state,zip_code,phone_number,email_id):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.phone_number = phone_number
+        self.email_id = email_id
+        
+    def __str__(self):
+        return (
+        f"First Name   : {self.first_name}\n"
+        f"Last Name    : {self.last_name}\n"
+        f"Address      : {self.address}\n"
+        f"City         : {self.city}\n"
+        f"State        : {self.state}\n"
+        f"Zip Code     : {self.zip_code}\n"
+        f"Phone Number : {self.phone_number}\n"
+        f"Email ID     : {self.email_id}\n"
+        "---------------------------"
+    )
---
+++ main.py
@@ -0,0 +1,8 @@
+from contacts import Contacts
+
+if __name__ == '__main__':
+    print("Welcome to Address Book Program - UC1")
+    
+    contact1 = Contacts("Lynda","honest","123 Main St","Chennai","Tamil Nadu","600001","9876543210","lynda@example.com")
+    
+    print(contact1)
