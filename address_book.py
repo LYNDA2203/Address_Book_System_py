f0b82813ec50c56414e2b64bf403c5dcfc567a85
LYNDA2203 <lindam.rita@gmail.com>
2025-07-08 14:25:31 +0530

    [LYNDA] Feat: Added the method of edit contact in the address book class

--- address_book.py
+++ address_book.py
@@ -8,6 +8,28 @@ class AddressBook:
     #method to append the contact to address book
     def add_contacts(self,contacts:Contacts):
         self.contact.append(contacts)
+    
+    #method to find wheather the contact is present in addressbook
+    def find_contacts(self,first_name,last_name):
+        for contacts in self.contact:
+            if contacts.first_name == first_name and contacts.last_name == last_name:
+                return contacts
+        return None
+    
+    #Method to edit the contact that we found in find_contact method
+    def edit_contacts(self,first_name,last_name):
+        contact = self.find_contacts(first_name,last_name)
+        if contact:
+            contact.address = input("New Address: ")
+            contact.city = input("New City: ")
+            contact.state = input("New State: ")
+            contact.zip_code = input("New Zip Code: ")
+            contact.phone = input("New Phone: ")
+            contact.email = input("New Email: ")
+            print("\n Contact updated successfully!\n")
+        else:
+            print("\n Contact not found....")
+            
         
     #Method to display the details of contact       
     def display_contacts(self):
--- main.py
+++ main.py
@@ -4,11 +4,17 @@ from address_book import AddressBook
 if __name__ == '__main__':
     book = AddressBook()
     print("Welcome to Address Book Program - UC1")
-    
+
     contact1 = Contacts("Lynda","honest","123 Main St","Chennai","Tamil Nadu","600001","9876543210","lynda@example.com")
     contact2 = Contacts("princy","honest","72,ranganathan street","Chennai","Tamil Nadu","600011","9876543211","princy@example.com")
-    
+
     book.add_contacts(contact1)
     book.add_contacts(contact2)
-    
+
+    print("Old Contact details: ")
+    book.display_contacts()
+    book.find_contacts("Lynda","honest")
+
+    print("New Modified contact details: ")
+    book.edit_contacts("Lynda","honest")
     book.display_contacts()
