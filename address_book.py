2600fe3aa89a5bd7025181e3a672048d1ba1b6b9
LYNDA2203 <lindam.rita@gmail.com>
2025-07-08 15:14:22 +0530

    [LYNDA] feat:Added the new method to delete the contacts in address book

--- address_book.py
+++ address_book.py
@@ -29,8 +29,16 @@ class AddressBook:
             print("\n Contact updated successfully!\n")
         else:
             print("\n Contact not found....")
-            
-        
+      
+    #method to delete the contact in the address book
+    def delete_contacts(self,first_name,last_name):
+              contacts = self.find_contacts(first_name,last_name)
+              if contacts:
+                self.contact.remove(contacts)
+                print(f"\n Contact {first_name} {last_name} deleted successfully!\n")
+              else:
+                print(" Contact not found!")
+
     #Method to display the details of contact       
     def display_contacts(self):
         for cont in self.contact:
--- main.py
+++ main.py
@@ -5,16 +5,25 @@ if __name__ == '__main__':
     book = AddressBook()
     print("Welcome to Address Book Program - UC1")
 
-    contact1 = Contacts("Lynda","honest","123 Main St","Chennai","Tamil Nadu","600001","9876543210","lynda@example.com")
-    contact2 = Contacts("princy","honest","72,ranganathan street","Chennai","Tamil Nadu","600011","9876543211","princy@example.com")
+    book.add_contacts(Contacts("Lynda","honest","123 Main St","Chennai","Tamil Nadu","600001","9876543210","lynda@example.com"))
+    book.add_contacts(Contacts("princy","honest","72,ranganathan street","Chennai","Tamil Nadu","600011","9876543211","princy@example.com"))
 
-    book.add_contacts(contact1)
-    book.add_contacts(contact2)
-
-    print("Old Contact details: ")
+    
+    print("Display Original Contact details : ")
     book.display_contacts()
     book.find_contacts("Lynda","honest")
+    
 
-    print("New Modified contact details: ")
-    book.edit_contacts("Lynda","honest")
+    # print("New Updated contact details: ")
+    # book.edit_contacts("Lynda","honest")
+    # book.display_contacts()
+    
+    print("\nDelete Contact")
+    first_name = input("Enter First Name of contact to delete: ")
+    last_name = input("Enter Last Name: ")
+
+    book.delete_contacts(first_name, last_name)
+
+    print("Updated Contact List:")
     book.display_contacts()
+
